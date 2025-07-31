c91d747a66197b993de5674a269d8d8b8f272458
69amirhossein69 <a.h.khalaje@gmail.com>
2025-07-31 21:23:38 +0330

    feat: fetch and display cabin data from API and add loading spinner

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      new URL(
+        "https://zhilwbvzrspnxqoehcuz.supabase.co/storage/v1/object/public/cabin-images/**"
+      ),
+    ],
+  },
+};
 
 export default nextConfig;
