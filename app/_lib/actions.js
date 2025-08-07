9b47fba25754f5558f3b3fcd556520d796b94b1c
69amirhossein69 <a.h.khalaje@gmail.com>
2025-08-07 14:39:59 +0330

    feat: add delete reservations

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,12 +3,12 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 
 export async function updateGuest(formData) {
   const session = await auth();
-
-  
   if (!session) throw new Error("Unauthorized");
+
   const nationalID = formData.get("nationalID");
   const [nationality, countryFlag] = formData.get("nationality").split("%");
   if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID)) {
@@ -19,12 +19,28 @@ export async function updateGuest(formData) {
   const { data, error } = await supabase
     .from("guests")
     .update(updatedData)
-    .eq("id", session.user.guestId)
-
+    .eq("id", session.user.guestId);
 
   if (error) throw new Error("Guest could not be updated");
 
- revalidatePath("/account/profile");
+  revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("Unauthorized");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guesBookingIds = guestBookings.map((booking) => booking.id);
+  if (!guesBookingIds.includes(bookingId))
+    throw new Error("Unauthorized booking deletion");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+  if (error) throw new Error("Booking could not be deleted");
+  revalidatePath("/account/reservations");
 }
 
 export async function signInAction() {
